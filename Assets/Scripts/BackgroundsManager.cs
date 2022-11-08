4cf91f51eaa9403f0a4c20a74848201a344576a4
Alexandre Bourse <Alexandre.bourse@gmail.com>
2022-11-07 22:08:34 -0500

    Backgrounds improvements

--- Assets/Scripts/BackgroundsManager.cs
+++ Assets/Scripts/BackgroundsManager.cs
@@ -6,10 +6,13 @@ using UnityEngine;
 public class BackgroundsManager : MonoBehaviour
 {
     public static BackgroundsManager Instance { get; private set; }
-    public List<GameObject> typeOfBackgrounds;
-    private static int activeBackground = 0;
-    private static int numberOfActiveBackgroundIsInstanciated = 0;
+    public List<GameObject> TypeOfBackgrounds;
+    private static int ActiveBackground = 0;
+    private static int NumberOfActiveBackgroundIsInstanciated = 0;
+    private Vector3 BackGroundSpawn = new Vector3(18.9f, 0, 0);
     [SerializeField] private int NumberRepetitionBackground = 2;
+    private string PrefixName = "Background"; 
+
 
     
     private void Awake()
@@ -23,31 +26,37 @@ public class BackgroundsManager : MonoBehaviour
             Instance = this;
         }
 
-        GameObject firstBackgroud = Instantiate(typeOfBackgrounds[activeBackground], new Vector3(0, 0, 0), Quaternion.identity, GameObject.Find("Backgrounds").transform);
-        GameObject  secondBackgroud = Instantiate(typeOfBackgrounds[activeBackground], new Vector3(18.9f, 0, 0), Quaternion.identity, GameObject.Find("Backgrounds").transform);
-        numberOfActiveBackgroundIsInstanciated = 2;
-
+        GameObject firstBackgroud = Instantiate(TypeOfBackgrounds[ActiveBackground], new Vector3(0, 0, 0), Quaternion.identity, GameObject.Find("Backgrounds").transform);
+        NumberOfActiveBackgroundIsInstanciated = 1;
+        firstBackgroud.name = PrefixName + "Start";
+        createBackgroud();
 
     }
 
     public void createBackgroud()
     {
-        if( numberOfActiveBackgroundIsInstanciated == NumberRepetitionBackground)
-        {
-            numberOfActiveBackgroundIsInstanciated = 0;
-            activeBackground++;
-            if(activeBackground > typeOfBackgrounds.Count -1)
-            {
-                activeBackground = 0;
-            }
-        }
-        GameObject go = Instantiate(typeOfBackgrounds[activeBackground], new Vector3(18.9f, 0, 0), Quaternion.identity, GameObject.Find("Backgrounds").transform);        
-        go.name = "Background";
-        numberOfActiveBackgroundIsInstanciated++;
+        SelectActiveBackground();
+        GameObject go = Instantiate(TypeOfBackgrounds[ActiveBackground], BackGroundSpawn, Quaternion.identity, GameObject.Find("Backgrounds").transform);        
+        NumberOfActiveBackgroundIsInstanciated++;
+        go.name = PrefixName + NumberOfActiveBackgroundIsInstanciated%2;
     }
 
-    internal void DestroyBackground(GameObject background)
+    public void DestroyBackground(GameObject background)
     {
         Destroy(background);
+    }
+
+    private void SelectActiveBackground()
+    {
+        if (NumberOfActiveBackgroundIsInstanciated == NumberRepetitionBackground)
+        {
+            NumberOfActiveBackgroundIsInstanciated = 0;
+            ActiveBackground++;
+            if (ActiveBackground > TypeOfBackgrounds.Count - 1)
+            {
+                ActiveBackground = 0;
+            }
+        }
+
     }
 }
