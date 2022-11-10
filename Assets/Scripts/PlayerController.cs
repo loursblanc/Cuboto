07d2c4589ae6971cc97ed204eff37fa23b408741
Alexandre Bourse <Alexandre.bourse@gmail.com>
2022-11-09 20:35:34 -0500

    Changing the size of the CapsuleCollider2d when crouched
    -PlayerController become abstract
    -SwordmanController inherit  Player controller
    -SwordManController override     protected override CrounchDown() and     protected override void CrounchDownUP

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,30 +1,34 @@
 ﻿using System.Collections;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public abstract class PlayerController : MonoBehaviour
 {
 
     [Header("[Status]")]
     public bool IsGrounded;
-    [SerializeField] private bool IsCrouch = false;
+    [SerializeField] protected bool IsCrouch = false;
     public int CurrentJumpCount = 0;
 
     [Header("[Setting]")]
-    [SerializeField] private int JumpCount = 1;
-    [SerializeField] private float jumpForce = 2.75f;
+    [SerializeField] protected int JumpCount = 1;
+    public float jumpForce = 3f;
 
     protected Animator Anim;
     public Rigidbody2D PlayerControllerRigidbody2D;
+    protected CapsuleCollider2D playerCapsuleCollider2D;
+
 
 
     private void Awake()
     {
         Anim = this.transform.Find("SwordMan").GetComponent<Animator>();
         PlayerControllerRigidbody2D = this.GetComponent<Rigidbody2D>();
+        playerCapsuleCollider2D = this.GetComponent<CapsuleCollider2D>();
+
     }
 
 
-    private void Update()
+private void Update()
     {
         CheckInput();
     }
@@ -33,25 +37,40 @@ public class PlayerController : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.S))
         {
-            IsCrouch = true;
-            Anim.Play("Sit");
+            CrounchDown();
         }
         else if (Input.GetKeyUp(KeyCode.S))
         {
-            Anim.Play("Run");
-            IsCrouch = false;
+            CrounchUp();
         }
 
         if (Input.GetKeyDown(KeyCode.Space))     
         {
-
-            //TODO: Manage the case where the force is too high on the second jump
-            if (IsGrounded || CurrentJumpCount < JumpCount)
-            {
-                CurrentJumpCount++;
-                Anim.Play("Jump");
-                PlayerControllerRigidbody2D.AddForce(Vector2.up * (100 * (jumpForce)));
-            }
+            Jump();
         }
+    }
+
+    private void Jump()
+    {
+        //TODO: Manage the case where the force is too high on the second jump
+        if (IsGrounded || CurrentJumpCount < JumpCount)
+        {
+            CurrentJumpCount++;
+            Anim.Play("Jump");
+            PlayerControllerRigidbody2D.AddForce(Vector2.up * (100 * (jumpForce)));
+        }
+    }
+
+    
+    protected virtual void CrounchDown()
+    {
+        IsCrouch = true;
+        Anim.Play("Sit");
+    }
+
+    protected virtual void CrounchUp()
+    {
+        Anim.Play("Run");
+        IsCrouch = false;
     }
 }
---
+++ Assets/Scripts/SwordManController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordManController : PlayerController
+{
+
+    private float capsuleSizeYCrounch = 0.086f;
+    private float capsuleSizeYStand = 0.24f;
+    private float capsuleOffsetYCrounch = 1.077f;
+    private float capsuleOffsetYStand = 1.33f;
+
+
+    protected override void CrounchDown()
+    {
+        IsCrouch = true;
+        Anim.Play("Sit");
+        playerCapsuleCollider2D.offset = new Vector2(playerCapsuleCollider2D.offset.x, capsuleSizeYCrounch);
+        playerCapsuleCollider2D.size = new Vector2(playerCapsuleCollider2D.size.x, capsuleOffsetYCrounch);
+
+    }    
+
+
+    protected override void CrounchUp()
+    {
+        IsCrouch = false;
+        Anim.Play("Run");
+        playerCapsuleCollider2D.offset = new Vector2(playerCapsuleCollider2D.offset.x, capsuleSizeYStand);
+        playerCapsuleCollider2D.size = new Vector2(playerCapsuleCollider2D.size.x, capsuleOffsetYStand);
+    }
+
+
+}
