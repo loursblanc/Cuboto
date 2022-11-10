9e0cd2ad8b5ac56ad14af6aa0295aba4a262be84
Alexandre Bourse <Alexandre.bourse@gmail.com>
2022-11-10 13:11:51 -0500

    First enemy Animation + mouvment

---
+++ Assets/Scripts/Enemies.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemies : MonoBehaviour
+{
+
+    private Rigidbody2D _rigidbody2D;
+    public int speed = 5;
+
+    private void Awake()
+    {
+        _rigidbody2D = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        _rigidbody2D.AddForce((Vector2.left * 0.1f), ForceMode2D.Impulse);
+    }
+}
