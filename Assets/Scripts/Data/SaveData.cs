9272e4bf9608137869eea72202780333610d2dfa
Alexandre Bourse <Alexandre.bourse@gmail.com>
2022-11-17 09:38:28 -0500

    Config Build

--- Assets/Scripts/Data/SaveData.cs
+++ Assets/Scripts/Data/SaveData.cs
@@ -23,25 +23,28 @@ public class SaveData
         //highScore.PlayerName = "PlayerNameTest1";
         //highScore.Score = 1;
         //scoreData.highScores.Add(highScore);
-        List<HighScore> highScores = MainManager.Instance.HighScores;
+       //if(MainManager.Instance.HighScores != null) { 
+            List<HighScore> highScores = MainManager.Instance.HighScores;
         
-        highScores.Add(new HighScore(MainManager.Instance.PlayerName, currentScore));
+            highScores.Add(new HighScore(MainManager.Instance.PlayerName, currentScore));
 
-        if(highScores.Count > 1)
-        {
-            highScores.Sort((p1, p2) => p1.Score.CompareTo(p2.Score));
-        }
+            if(highScores.Count > 1)
+            {
+                highScores.Sort((p1, p2) => p1.Score.CompareTo(p2.Score));
+            }
 
-        if(highScores.Count > 10)
-        {
-            //removes the lowest score
-            highScores.RemoveAt(0);
-        }
+            if(highScores.Count > 10)
+            {
+                //removes the lowest score
+                highScores.RemoveAt(0);
+            }
 
-        //To do because of the limitation of the lists in the json by unity
-        scoreData.highScores = highScores;
-        string json = JsonUtility.ToJson(scoreData);
-        File.WriteAllText(SaveHighScorefilePath, json);
+            //To do because of the limitation of the lists in the json by unity
+            scoreData.highScores = highScores;
+            string json = JsonUtility.ToJson(scoreData);
+            File.WriteAllText(SaveHighScorefilePath, json);
+            MainManager.Instance.HighScores = scoreData.highScores;
+        //}
     }
 
     public List<HighScore> LoadHighScores()
